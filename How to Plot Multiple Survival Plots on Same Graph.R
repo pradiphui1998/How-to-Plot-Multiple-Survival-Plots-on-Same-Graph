6751d4fde513b9ea4aa1a75eb493863d17c92bdf
Pradip Hui <89596298+pradiphui1998@users.noreply.github.com>
2023-09-02 11:52:59 +0530

    Add files via upload

---
+++ How to Plot Multiple Survival Plots on Same Graph.R
@@ -0,0 +1,24 @@
+# Load the required packages
+library(survival)
+library(survminer)
+
+# Create two survival curves
+fit1 <- survfit(Surv(time, status) ~ sex, data = lung)
+p1=ggsurvplot(fit1)
+
+fit2 <- survfit(Surv(time, status) ~ ph.ecog, data = lung)
+p2=ggsurvplot(fit2)
+
+fit3 <- survfit(Surv(time, status) ~ sex, data = lung)
+p3=ggsurvplot(fit3)
+
+fit4 <- survfit(Surv(time, status) ~ ph.ecog, data = lung)
+p4=ggsurvplot(fit4)
+
+splots <- list(p1,p2,p3,p4)
+
+# Arrange multiple ggsurvplots and print the output
+arrange_ggsurvplots(splots, print = TRUE,
+                    ncol = 2, nrow = 2)
+
+
